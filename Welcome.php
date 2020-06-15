eef3b810a4e798291ddb33e4acd32316de90a0f8
Summaiya362 <63764819+Summaiya362@users.noreply.github.com>
2020-06-15 20:47:34 +0700

    controler>models>config
     untuk login dan logout admin

---
+++ Welcome.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('form', 'url');
+		$this->load->library('form_validation');
+
+		// Load Model
+		$this->load->model('welcome_model');
+	}
+	public function index()
+	{
+		$this->load->view('welcome_message');
+		//$this->load->view('template/footer');
+	}
+	public function saved()
+	{
+		$e = $this->input->post('form-verify');
+		if (isset($e)) {
+			$a = $this->input->post('form-name');
+			$b = $this->input->post('form-date');
+			$c = $this->input->post('form-time');
+			$d = $this->input->post('form-type');
+			$data = array(
+				'nama_register' => $a,
+				'date_register' => $b,
+				'time_register' => $c,
+				'id_pangkas' => $d,
+				'status' => 'N',
+			);
+			$this->welcome_model->saved($data);
+			$this->load->view('congrats', $data);
+		} else {
+			redirect('/');
+		}
+	}
+}
